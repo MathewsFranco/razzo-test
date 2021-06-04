1a829a93ed40be810989ed8cf717a9cc49f50fe1
Mathews Franco <mathewsandre.franco@gmail.com>
2021-06-04 18:34:59 -0300

    set restaurant Name inside cartItem

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from '../Counter';
 import * as Styled from './style';
 import formatPrice from '../../services/formatPrice';
 import { useDispatch } from 'react-redux';
 import { deleteCartItem } from '../../store/ducks/cart';
+import restaurantsApi from '../../services/restaurantsApi';
 
-const CartItem = ({ img, name, type, price, quantity, uniqueId }) => {
+const CartItem = ({
+  img,
+  name,
+  type,
+  price,
+  quantity,
+  uniqueId,
+  businessId,
+}) => {
   const dispatch = useDispatch();
   function handleDelete() {
     dispatch(deleteCartItem(uniqueId));
   }
+
+  const [businessName, setBusinessName] = useState('Restaurant Name');
+
+  restaurantsApi
+    .get(businessId)
+    .then((res) => setBusinessName(res.data.name))
+    .catch(console.log);
+
   return (
     <Styled.CartItemCard>
-      <Styled.CartItemRestaurant>Teste Top</Styled.CartItemRestaurant>
+      <Styled.CartItemRestaurant>{businessName}</Styled.CartItemRestaurant>
       <Styled.CartItemInfo>
         <Styled.Wrapper>
           <Styled.CartItemImage src={img} />
